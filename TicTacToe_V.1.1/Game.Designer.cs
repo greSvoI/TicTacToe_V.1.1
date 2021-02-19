762e9cbea4f12c20cfc769ebb9a4a6f549ef2806
Сергей <serega.iva@outlook.com>
2021-02-19 15:00:54 +0300

    Добавьте файлы проекта.

---
+++ TicTacToe_V.1.1/Game.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace TicTacToe_V._1._1
+{
+	partial class Game
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.buttonReload = new System.Windows.Forms.Button();
+			this.B1 = new System.Windows.Forms.Button();
+			this.B2 = new System.Windows.Forms.Button();
+			this.B3 = new System.Windows.Forms.Button();
+			this.B4 = new System.Windows.Forms.Button();
+			this.B5 = new System.Windows.Forms.Button();
+			this.B6 = new System.Windows.Forms.Button();
+			this.B7 = new System.Windows.Forms.Button();
+			this.B8 = new System.Windows.Forms.Button();
+			this.B9 = new System.Windows.Forms.Button();
+			this.textBoxIP = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.buttonConnect = new System.Windows.Forms.Button();
+			this.buttonCreate = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(13, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(319, 26);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// buttonReload
+			// 
+			this.buttonReload.Location = new System.Drawing.Point(232, 393);
+			this.buttonReload.Name = "buttonReload";
+			this.buttonReload.Size = new System.Drawing.Size(100, 23);
+			this.buttonReload.TabIndex = 1;
+			this.buttonReload.Text = "Рестарт";
+			this.buttonReload.UseVisualStyleBackColor = true;
+			this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+			// 
+			// B1
+			// 
+			this.B1.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B1.Location = new System.Drawing.Point(13, 67);
+			this.B1.Margin = new System.Windows.Forms.Padding(0);
+			this.B1.Name = "B1";
+			this.B1.Size = new System.Drawing.Size(100, 89);
+			this.B1.TabIndex = 2;
+			this.B1.UseVisualStyleBackColor = true;
+			// 
+			// B2
+			// 
+			this.B2.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B2.Location = new System.Drawing.Point(122, 67);
+			this.B2.Margin = new System.Windows.Forms.Padding(0);
+			this.B2.Name = "B2";
+			this.B2.Size = new System.Drawing.Size(100, 89);
+			this.B2.TabIndex = 3;
+			this.B2.UseVisualStyleBackColor = true;
+			// 
+			// B3
+			// 
+			this.B3.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B3.Location = new System.Drawing.Point(232, 67);
+			this.B3.Margin = new System.Windows.Forms.Padding(0);
+			this.B3.Name = "B3";
+			this.B3.Size = new System.Drawing.Size(100, 89);
+			this.B3.TabIndex = 4;
+			this.B3.UseVisualStyleBackColor = true;
+			// 
+			// B4
+			// 
+			this.B4.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B4.Location = new System.Drawing.Point(13, 166);
+			this.B4.Margin = new System.Windows.Forms.Padding(0);
+			this.B4.Name = "B4";
+			this.B4.Size = new System.Drawing.Size(100, 89);
+			this.B4.TabIndex = 5;
+			this.B4.UseVisualStyleBackColor = true;
+			// 
+			// B5
+			// 
+			this.B5.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B5.Location = new System.Drawing.Point(122, 166);
+			this.B5.Margin = new System.Windows.Forms.Padding(0);
+			this.B5.Name = "B5";
+			this.B5.Size = new System.Drawing.Size(100, 89);
+			this.B5.TabIndex = 6;
+			this.B5.UseVisualStyleBackColor = true;
+			// 
+			// B6
+			// 
+			this.B6.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B6.Location = new System.Drawing.Point(232, 166);
+			this.B6.Margin = new System.Windows.Forms.Padding(0);
+			this.B6.Name = "B6";
+			this.B6.Size = new System.Drawing.Size(100, 89);
+			this.B6.TabIndex = 7;
+			this.B6.UseVisualStyleBackColor = true;
+			// 
+			// B7
+			// 
+			this.B7.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B7.Location = new System.Drawing.Point(13, 266);
+			this.B7.Margin = new System.Windows.Forms.Padding(0);
+			this.B7.Name = "B7";
+			this.B7.Size = new System.Drawing.Size(100, 89);
+			this.B7.TabIndex = 8;
+			this.B7.UseVisualStyleBackColor = true;
+			// 
+			// B8
+			// 
+			this.B8.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B8.Location = new System.Drawing.Point(122, 266);
+			this.B8.Margin = new System.Windows.Forms.Padding(0);
+			this.B8.Name = "B8";
+			this.B8.Size = new System.Drawing.Size(100, 89);
+			this.B8.TabIndex = 9;
+			this.B8.UseVisualStyleBackColor = true;
+			// 
+			// B9
+			// 
+			this.B9.Font = new System.Drawing.Font("Bookman Old Style", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.B9.Location = new System.Drawing.Point(232, 266);
+			this.B9.Margin = new System.Windows.Forms.Padding(0);
+			this.B9.Name = "B9";
+			this.B9.Size = new System.Drawing.Size(100, 89);
+			this.B9.TabIndex = 10;
+			this.B9.UseVisualStyleBackColor = true;
+			// 
+			// textBoxIP
+			// 
+			this.textBoxIP.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBoxIP.Location = new System.Drawing.Point(55, 361);
+			this.textBoxIP.Name = "textBoxIP";
+			this.textBoxIP.Size = new System.Drawing.Size(277, 26);
+			this.textBoxIP.TabIndex = 11;
+			this.textBoxIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 360);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 24);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "IP";
+			// 
+			// buttonConnect
+			// 
+			this.buttonConnect.Location = new System.Drawing.Point(122, 393);
+			this.buttonConnect.Name = "buttonConnect";
+			this.buttonConnect.Size = new System.Drawing.Size(100, 23);
+			this.buttonConnect.TabIndex = 13;
+			this.buttonConnect.Text = "Подключить";
+			this.buttonConnect.UseVisualStyleBackColor = true;
+			this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+			// 
+			// buttonCreate
+			// 
+			this.buttonCreate.Location = new System.Drawing.Point(16, 393);
+			this.buttonCreate.Name = "buttonCreate";
+			this.buttonCreate.Size = new System.Drawing.Size(100, 23);
+			this.buttonCreate.TabIndex = 14;
+			this.buttonCreate.Text = "Создать";
+			this.buttonCreate.UseVisualStyleBackColor = true;
+			this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+			// 
+			// Game
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(340, 424);
+			this.Controls.Add(this.buttonCreate);
+			this.Controls.Add(this.buttonConnect);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxIP);
+			this.Controls.Add(this.B9);
+			this.Controls.Add(this.B8);
+			this.Controls.Add(this.B7);
+			this.Controls.Add(this.B6);
+			this.Controls.Add(this.B5);
+			this.Controls.Add(this.B4);
+			this.Controls.Add(this.B3);
+			this.Controls.Add(this.B2);
+			this.Controls.Add(this.B1);
+			this.Controls.Add(this.buttonReload);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Game";
+			this.Text = "Game";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Game_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button buttonReload;
+		private System.Windows.Forms.Button B1;
+		private System.Windows.Forms.Button B2;
+		private System.Windows.Forms.Button B3;
+		private System.Windows.Forms.Button B4;
+		private System.Windows.Forms.Button B5;
+		private System.Windows.Forms.Button B6;
+		private System.Windows.Forms.Button B7;
+		private System.Windows.Forms.Button B8;
+		private System.Windows.Forms.Button B9;
+		private System.Windows.Forms.TextBox textBoxIP;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button buttonConnect;
+		private System.Windows.Forms.Button buttonCreate;
+	}
+}
---
+++ TicTacToe_V.1.1/Game.cs
@@ -0,0 +1,265 @@
+﻿using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_V._1._1
+{
+	public partial class Game : Form
+	{
+		private char Player;
+		private char Opponent;
+
+		IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+
+		public TcpListener tcpListener;
+
+		TcpClient tcpClient = new TcpClient();
+		NetworkStream networkStream;
+
+		public Game()
+		{
+			InitializeComponent();
+			ButtonDisabled();
+			foreach(Control control in Controls)
+				if(control is Button)
+					if(control.Name.Length<3)
+				control.Click += Control_Click;
+		}
+		private void GameStream()
+		{
+			if (Chek()) return;
+			ButtonDisabled();
+			this.textBox1.Text = "Ход противника";
+			//Thread thread = new Thread(new ThreadStart(GetMsg));
+			//thread.Start();
+			GetMsg();
+			this.textBox1.Text = "Ваш ход";
+			if (!Chek())
+				ButtonEnabled();
+		}
+		private void GetMsg()
+		{
+			try
+			{
+				while (true)
+				{
+					byte[] getData = new byte[64];
+					StringBuilder stringBuilder = new StringBuilder();
+					int size = 0;
+					do
+					{
+						size = networkStream.Read(getData, 0, getData.Length);
+						stringBuilder.Append(Encoding.UTF8.GetString(getData, 0, size));
+
+					} while (networkStream.DataAvailable);
+
+					string temp = stringBuilder.ToString();
+
+
+					foreach (Control control in Controls)
+						if (control is Button && control.Name.Length < 3)
+							if (control.Name == temp)
+								//control.Text = Opponent.ToString();
+								control.Invoke(new Action(() =>
+								{
+									control.Text = Opponent.ToString();
+									textBox1.Text = "Ваш ход";
+									if (!Chek())
+										ButtonEnabled();
+								}));
+				}
+				//textBox1.Invoke(new Action(() => textBox1.Text = "Ваш ход"));
+				//textBox1.Text = "Ваш ход";
+				//ButtonEnabled();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + " GetMsg");
+				tcpListener?.Stop();
+				tcpClient?.Close();
+				networkStream?.Close();
+			}
+		}
+		private void Control_Click(object sender, System.EventArgs e)
+		{
+			Button button = (Button)sender;
+			foreach (Control control in Controls)
+				if (control is Button)
+					if (control.Name == button.Name)
+					{
+						control.Text = Player.ToString(); 
+						this.textBox1.Text = "Ход противника";
+					}
+
+			byte[] data = Encoding.UTF8.GetBytes(button.Name);
+			networkStream.Write(data, 0, data.Length);
+
+			if (Chek()) return;
+			ButtonDisabled();
+			
+			//GameStream();
+		}
+
+		private void Game_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			networkStream?.Close();
+			tcpClient?.Close();
+		}
+		private void ButtonDisabled()
+		{
+			
+			foreach (Control control in Controls)
+				if (control is Button)
+					if (control.Name.Length < 3)
+						control.Enabled = false;
+		}
+		private void ButtonEnabled()
+		{
+				foreach (Control control in Controls)
+				if (control is Button)
+					if (control.Name.Length < 3 && control.Text == "")
+						control.Enabled = true;
+		}
+
+		private void buttonCreate_Click(object sender, System.EventArgs e)
+		{
+			Player = 'X';
+			Opponent = 'O';
+
+			try
+			{
+				if (textBoxIP.Text == "")
+				{
+
+					tcpListener = new TcpListener(iPAddress, 8000);
+					tcpListener.Start();
+				    // Connect();
+					tcpClient = tcpListener.AcceptTcpClient();
+					//Thread start = new Thread(new ThreadStart(Connect));
+					//start.Start();
+					//tcpClient = tcpListener.AcceptTcpClient();
+					networkStream = tcpClient.GetStream();
+					textBox1.Text = "Ваш ход";
+					ButtonEnabled();
+					Thread thread = new Thread(new ThreadStart(GetMsg));
+					thread.Start();
+
+
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + " buttonCreate_Click");
+				tcpListener?.Stop();
+				tcpClient?.Close();
+				networkStream?.Close();
+			}
+
+			
+		}
+		private async void Connect()
+		{
+			await Task.Run(() => tcpClient = tcpListener.AcceptTcpClient());
+			
+			networkStream = tcpClient.GetStream();
+			textBox1.Text = "Ваш ход";
+			Thread thread = new Thread(new ThreadStart(GetMsg));
+			thread.Start();
+			ButtonEnabled();
+		}
+		private void buttonConnect_Click(object sender, EventArgs e)
+		{
+			Player = 'O';
+			Opponent = 'X';
+			try
+			{
+				tcpClient = new TcpClient();
+				tcpClient.Connect("127.0.0.1", 8000);
+				networkStream = tcpClient.GetStream();
+				Thread thread = new Thread(new ThreadStart(GetMsg));
+				thread.Start();
+				//ButtonDisabled();
+				//this.textBox1.Text = "Ход противника";
+				//GameStream();
+			}
+			catch (Exception ex)
+			{
+				networkStream?.Close();
+				tcpClient?.Close();
+				MessageBox.Show(ex.Message+ " buttonConnect_Click");
+			}
+		}
+		private bool Chek()
+		{
+			//Горизонталь
+			if (B1.Text == B2.Text && B2.Text == B3.Text && B3.Text != "")
+			{
+				Win(B1);
+				return true;
+			}
+			else if (B4.Text == B5.Text && B5.Text == B6.Text && B6.Text != "")
+			{
+				Win(B4);
+				return true;
+			}
+			else if (B7.Text == B8.Text && B8.Text == B9.Text && B9.Text != "")
+			{
+				Win(B7);
+				return true;
+			}
+			//Диагональ
+			else if (B1.Text == B5.Text && B5.Text == B9.Text && B9.Text != "")
+			{
+				Win(B1);
+				return true;
+			}
+			else if (B3.Text == B5.Text && B5.Text == B7.Text && B7.Text != "")
+			{
+				Win(B3);
+				return true;
+			}
+			//Вертикаль
+			else if (B1.Text == B4.Text && B4.Text == B7.Text && B7.Text != "")
+			{
+				Win(B1);
+				return true;
+			}
+			else if (B2.Text == B5.Text && B5.Text == B8.Text && B8.Text != "")
+			{
+				Win(B2);
+				return true;
+			}
+			else if (B3.Text == B6.Text && B6.Text == B9.Text && B9.Text != "")
+			{
+				Win(B3);
+				return true;
+			}
+			int flag = 0;
+			foreach (Control item in Controls)
+				if (item is Button&&item.Name.Length<3)
+					if (item.Text != "")
+						flag++;
+			if (flag == 9) { MessageBox.Show("Ничья"); return true; }
+
+
+			return false;
+		}
+		private void Win(Button button)
+		{
+			if (button.Text[0] == Player)
+				MessageBox.Show("Вы победили");
+			else MessageBox.Show("Вы проиграли");
+		}
+
+		private void buttonReload_Click(object sender, EventArgs e)
+		{
+			foreach (Control control in Controls)
+				if (control is Button && control.Name.Length < 3)
+					control.Text = "";
+		}
+	}
+}
